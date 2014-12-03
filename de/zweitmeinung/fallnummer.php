078a10f396c70616b2565c9ce3b4de92efed15e1
Patrick Federi <patrick@federi.com>
2014-12-03 08:03:20 +0100

    Struktur und Navigation
    Struktur für weitere Unterseiten vorbereitet. Navigation für
    Zweitmeinungsseite begonnen.

---
+++ de/zweitmeinung/fallnummer.php
@@ -0,0 +1,31 @@
+<?php include("../../includes/header.inc.php"); ?>
+
+<div class="row">
+	<div class="large-2 medium-2 small-1 columns">
+		<ul id="lstzweitmmeinung">
+			<li><a href="/zweitmeinung/fallnummer">Ihre Fallnummer<span>9876321-1</span></a>
+				<ul>
+					<li><a href="#">Speichern</a></li>
+					<li><a href="#">Löschen</a></li>
+				</ul>
+			</li>
+			<li><a href="/zweitmeinung/bestellen">Zweitmeinung bestellen</a></li>
+			<li><a href="/zweitmeinung/dateneingabe">Daten eingeben</a></li>
+			<li><a href="/zweitmeinung/datenprüfung">Daten werden geprüft</a></li>
+			<li><a href="/zweitmeinung/spezialistenauswahl">Spezialist auswählen</a></li>
+			<li><a href="/zweitmeinung/gespraech">Ihr Zweitmeinungsgespräch</a></li>
+			<li><a href="/zweitmeinung/befund">Ihr Befund</a></li>
+		</ul>
+	</div>
+	<div class="large-10 medium-10 small-11 columns">
+	</div>
+</div>
+
+
+
+
+
+
+
+
+<?php include("../../includes/footer.inc.php"); ?>
