0e5c53a13cdcf5f30f04971a75d6d6f03e53bf2c
Patrick Federi <patrick@federi.com>
2014-12-05 14:10:40 +0100

    CSS

---
+++ includes/stepbystep.inc.php
@@ -0,0 +1,16 @@
+<div class="large-3 medium-3 small-1 columns stepbystep">
+	<ul id="lstZweitmeinung">
+		<li><img src="/images/logos/surgeon-sw.png" alt="Surgeon" />Ihre Fallnummer<br><span>9876321-1</span>
+			<ul>
+				<li><a href="#">Speichern</a></li>
+				<li><a href="#">Löschen</a></li>
+			</ul>
+		</li>
+		<li><a href="/zweitmeinung/bestellen" <?php if ($_SERVER['REQUEST_URI']=='/zweitmeinung/bestellen' ) {echo 'class="selected"';} ?>>Zweitmeinung bestellen</a><progress max="100" value="80"></progress></li>
+		<li><a href="/zweitmeinung/dateneingabe">Daten eingeben</a><progress max="100" value="0"></progress></li>
+		<li><a href="/zweitmeinung/datenprüfung">Daten werden geprüft</a><progress max="100" value="0"></progress></li>
+		<li><a href="/zweitmeinung/spezialistenauswahl">Spezialist auswählen</a><progress max="100" value="0"></progress></li>
+		<li><a href="/zweitmeinung/gespraech">Ihr Zweitmeinungsgespräch</a><progress max="100" value="0"></progress></li>
+		<li><a href="/zweitmeinung/befund">Ihr Befund</a><progress max="100" value="0"></progress></li>
+	</ul>
+</div>
