70c9b086357297fda19f27aa40e7d26ba12997d6
dejot <dajergovic@gmail.com>
2014-12-05 14:49:29 +0100

    Layot.css geändert _darko.scss hinzugekommen bei den Includes login & reg geändert

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,19 +1,31 @@
 <form id="login" action="" method="" accept-charset="utf-8" data-abide>
 	<div class="email-field">
-		<label>Ihre E-Mail Adresse <small>(Pflichtfeld)</small></label>
-		<input type="email" required placeholder="mail@anbieter.ch">
-		<small class="error">Wir benötigen eine gültige E-Mailadresse, um für Sie ein Profil anzulegen.</small>
+
+		<div class="row">
+			<div class="large-10 columns">
+				<label>Ihre E-Mail Adresse <small>(Pflichtfeld)</small></label>
+				<input type="email" required placeholder="mail@anbieter.ch">
+				<small class="error">Wir benötigen eine gültige E-Mailadresse, um für Sie ein Profil anzulegen.</small>			
+			</div>
+		</div>
+
 	</div>
 	<div class="email-field">
-		<label>Ihre Mobile Telefonnummer <small>(Pflichtfeld)</small></label>
-		<input type="tel" required placeholder="079 123 45 67">
-		<small class="error">Um Ihnen Zugriff auf die Plattform zu gewähren, benötigen wir eine gültige Natel-Nummer</small>
+		<div class="row">
+			<div class="large-10 columns">
+				<label>Ihre Mobile Telefonnummer <small>(Pflichtfeld)</small></label>
+				<input type="tel" required placeholder="079 123 45 67">
+				<small class="error">Um Ihnen Zugriff auf die Plattform zu gewähren, benötigen wir eine gültige Natel-Nummer</small>					
+			</div>
+		</div>
+
+
 	</div>
 	<div class="row">
-		<div class="large-9 columns">
-			<button type="submit" id="loginWeiter">Ja, Zweitmeinung bestellen</button>
+		<div class="large-6 columns text-center">
+			<button class="button expand" type="submit" id="loginWeiter">Login</button>
 		</div>
-		<div class="large-3 columns">
+		<div class="large-6 columns text-center">
 			<a href="#" class="button expand white">Abbrechen</a>
 		</div>
 	</div>
--- includes/reg.inc.php
+++ includes/reg.inc.php
@@ -1,19 +1,28 @@
 <form id="login" action="" method="" accept-charset="utf-8" data-abide>
 	<div class="email-field">
-		<label>Ihre Fallnummer <small>(Pflichtfeld)</small></label>
-		<input type="number" required placeholder="3487655.076">
-		<small class="error">Wir benötigen eine gültige Fallnummer, um richtig identifizeren zu können.</small>
+		<div class="row">
+			<div class="large-10 columns">
+				<label>Ihre Fallnummer <small>(Pflichtfeld)</small></label>
+				<input type="number" required placeholder="3487655.076">
+				<small class="error">Wir benötigen eine gültige Fallnummer, um richtig identifizeren zu können.</small>
+			</div>
+		</div>
 	</div>
 	<div class="email-field">
-		<label>Ihre E-Mail Adresse <small>(Pflichtfeld)</small></label>
-		<input type="email" required placeholder="mail@anbieter.ch">
-		<small class="error">Wir benötigen eine gültige Fallnummer, um richtig identifizeren zu können.</small>
+		<div class="row">
+			<div class="large-10 columns">
+				<label>Ihre E-Mail Adresse <small>(Pflichtfeld)</small></label>
+				<input type="email" required placeholder="mail@anbieter.ch">
+				<small class="error">Wir benötigen eine gültige Fallnummer, um richtig identifizeren zu können.</small>				
+			</div>
+		</div>
+
 	</div>
 	<div class="row">
-		<div class="large-9 columns">
-			<button type="submit" id="loginWeiter">Ja, Zweitmeinung bestellen</button>
+		<div class="large-6 columns">
+			<button class="button expand" type="submit" id="loginWeiter">Ja, Zweitmeinung bestellen</button>
 		</div>
-		<div class="large-3 columns">
+		<div class="large-6 columns">
 			<a href="#" class="button expand white">Abbrechen</a>
 		</div>
 	</div>
