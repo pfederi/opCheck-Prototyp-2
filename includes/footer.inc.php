79164686af8e16e7532c7838bd2b133fd3431ef3
katering <kate@katering.de>
2014-12-05 11:00:10 +0100

    Footer angepasst
    Zertifizierung (Bilder, Links)

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,18 +5,29 @@
 		Birmensdorferstrasse 470<br>
 		8055 Zürich<br>
 		Schweiz</p>
-	</div>
-	<div class="large-4 medium-6 small-6 columns">
 		<p><strong>Kontaktieren Sie uns</strong><br>
 		Telefon: 044 123 45 67<br>
 		<a href="mailto:info@opcheck.ch">info@opcheck.ch</a></p>
 	</div>
 	<div class="large-4 medium-6 small-6 columns">
+		<p><strong>opCheck ist zertifiziert</p>
+		<p><a href="http://www.swissmadesoftware.org/"><img src="/images/logos/swiss_made_software.jpg" alt="check"></a> &nbsp; &nbsp;
+		<a href="http://www.hon.ch/HONcode/Patients/intro_de.html"><img src="/images/logos/hon-code.png" alt="check"></a></p>
+	</div>
+	<div class="large-4 medium-6 small-6 columns">
+		<p><strong>Weitere Informationen</strong><br>
+			<a href="faq">FAQ</a><br>
+			<a href="datenschutz-richtlinien">Datenschutz-Richtlinien</a><br>
+			<a href="agb">AGB</a><br>
+			<a href="impressum">Impressum</a>
+		</p>
+		<!--
 		<ul>
 			<li><a href="faq">FAQ</a></li>
 			<li><a href="datenschutz-richtlinien">Datenschutz-Richtlinien</a></li>
 			<li><a href="impressum">Impressum</a></li>
 		</ul>
+		-->
 	</div>
 </footer>
 
