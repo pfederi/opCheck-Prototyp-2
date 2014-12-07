45bf780efe2972b1285aa6d5cd0f703971e84349
katering <kate@katering.de>
2014-12-07 21:44:07 +0100

    Kurzer Text für Datenprüfung

--- de/zweitmeinung/datenpruefung.php
+++ de/zweitmeinung/datenpruefung.php
@@ -3,7 +3,13 @@
 <div class="row" data-equalizer>
 	<?php include("../../includes/stepbystep.inc.php"); ?>
 	<div class="large-9 medium-9 small-11 columns" data-equalizer-watch>
-		Text einfügen
+		<h1>Datenprüfung</h1>
+
+		<div class="row">
+			<div class="large-9 medium-9 small-11 columns" data-equalizer-watch>
+				<p>In der endgültigen Version stehen hier Angaben zum Stand der Zweitmeinung</p>
+			</div>
+		</div>
 	</div>
 </div>
 
