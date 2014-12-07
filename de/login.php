0920d3c51a5d39034d195e415d4ec0c99ae0aec3
Patrick Federi <patrick@federi.com>
2014-12-07 09:43:33 +0100

    tooltips im login richtig gesetzt

--- de/login.php
+++ de/login.php
@@ -8,16 +8,15 @@
 <!-- 	linke Seite bestimmt für die Registration, bzw weitmeinung einholen -->	
 	<div class="large-5 columns">
 		<h1>Registrieren</h1>
-		<p>Sie möchten eine Zweitmeinung bestellen.
-		<img data-tooltip aria-haspopup="true" class="tip-top" title="Richtig, wenn Sie Ihren Fall noch nicht bei uns hinterlegt haben. Oder wenn Sie eine bereits eine Zweitmeinung durch opCheck erhalten haben aber für eine andere Operation eine zweite Meinung wünschen." src="/images/icons/help.svg" alt="help" /></p>
+		<p>Sie möchten eine Zweitmeinung bestellen. <img data-tooltip aria-haspopup="true" class="tip-bottom" style="float:none" title="Richtig, wenn Sie Ihren Fall noch nicht bei uns hinterlegt haben. Oder wenn Sie eine bereits eine Zweitmeinung durch opCheck erhalten haben aber für eine andere Operation eine zweite Meinung wünschen." src="/images/icons/help.svg" alt="help" /></p>
+		
 		<p>Bitte geben Sie zunächst Ihre E-Mailadresse und Ihre Mobiltelefonnummer ein.</p>
 		<?php include("../includes/reg.inc.php"); ?>
 	</div>
 
 	<div class="large-5 columns">
 		<h1>Login</h1>
-		<p>Sie haben bereits eine Fallnummer erhalten und möchten auf Ihre Daten zugreifen.
-		<img data-tooltip aria-haspopup="true" class="tip-top" title="Ihre Fallnummer haben wir Ihnen per SMS sowie per Post mitgeteilt." src="/images/icons/help.svg" alt="help" /></p>
+		<p>Sie haben bereits eine Fallnummer erhalten und möchten auf Ihre Daten zugreifen. <img data-tooltip aria-haspopup="true" class="tip-bottom" style="float:none" title="Ihre Fallnummer haben wir Ihnen per SMS sowie per Post mitgeteilt." src="/images/icons/help.svg" alt="help" /></p>	
 		<p>Bitte identifizieren Sie sich mit der Angabe Ihrer Fallnummer und ihrer E-Mailadresse.</p>
 		<?php include("../includes/login.inc.php"); ?>
 	</div>
